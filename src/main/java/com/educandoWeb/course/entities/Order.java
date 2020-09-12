c516b2a8e57d23a04c3680f88a894616d89486ea
cwi.bschwertner <cwi.bschwertner@bv.com.br>
2020-09-12 17:35:37 -0300

    branch - Adicionando enum para Order

--- src/main/java/com/educandoWeb/course/entities/Order.java
+++ src/main/java/com/educandoWeb/course/entities/Order.java
@@ -1,9 +1,9 @@
 package com.educandoWeb.course.entities;
 
 
+import com.educandoWeb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
@@ -14,16 +14,12 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
@@ -32,15 +28,45 @@ public class Order implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
     private Long id;
+
 
     @JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",
         timezone = "GMT")
+    @Getter
+    @Setter
     private Instant moment;
+
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
+    @Getter
+    @Setter
     private User client;
+
+    //Utilizado o get desta maneira por que o orderStauts esta sendo transformado em Integer
+    //Para ficar compatível ao banco de dados
+    //Ou seja, enum para o java e Integer par ao banco de dados
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    //Utilizado o set desta maneira por que o orderStauts esta sendo transformado em Integer
+    //Para ficar compatível ao banco de dados
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
 
     @Override
     public boolean equals(Object o) {
