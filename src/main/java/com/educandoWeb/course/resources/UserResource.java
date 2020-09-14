1675c17984fb84d0b0420a2aefbba520b0b3ac16
cwi.bschwertner <cwi.bschwertner@bv.com.br>
2020-09-13 22:13:30 -0300

    Branch - Adicionando PostMapping de User

--- src/main/java/com/educandoWeb/course/resources/UserResource.java
+++ src/main/java/com/educandoWeb/course/resources/UserResource.java
@@ -2,13 +2,19 @@ package com.educandoWeb.course.resources;
 
 import com.educandoWeb.course.entities.User;
 import com.educandoWeb.course.services.UserService;
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.HeadersBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController //Controlador Rest
 @RequestMapping(value = "/users") // Para colocar o caminho do Path, ou nome do recurso
@@ -27,5 +33,16 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id){
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        .buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
+
+        //O tipo abaixo, não retorna a URl do Header
+        //return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 }
